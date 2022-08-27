fb0f6a458a4f1aa9c52265b5aeba078eb166f029
Kemosabe <joshuapearson1993gmail.com>
2022-08-28 00:09:23 +0100

    Made the background and it's cool

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Square } from './classes/square';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'solihul-25-plus';
+export class AppComponent implements OnInit {
+  squares = new Array<Square>();
+  colours = [
+    "#ffffff",
+    "#2f728d",
+    "#f2935d",
+    "#40c0c0"
+  ]
+
+  ngOnInit() {
+    this.generateSquares();
+  }
+
+  private generateSquares() {
+    for (let i = 0; i < 1000; i++) {
+      let colourPicker = Array<string>();
+      this.colours.forEach(c => colourPicker.push(c));
+      this.squares.push(new Square(`square-${i}`, this.pickAColour(colourPicker), this.pickAColour(colourPicker)));
+    }
+  }
+
+  private pickAColour(colourPicker: Array<string>) {
+    const i = Math.floor(Math.random() * colourPicker.length - 1) + 1;
+    return colourPicker.splice(i, 1)[0];
+  }
 }
---
+++ src/app/classes/square.ts
@@ -0,0 +1,11 @@
+export class Square {
+    name: string;
+    colour1: string;
+    colour2: string;
+
+    constructor(name: string, colour1: string, colour2: string) {
+        this.name = name;
+        this.colour1 = colour1;
+        this.colour2 = colour2;
+    }
+}
