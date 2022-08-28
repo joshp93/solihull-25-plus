07453f33ff9238769782d426d0258281b8c3c487
Kemosabe <joshuapearson1993gmail.com>
2022-08-28 17:11:11 +0100

    Added basic content, still need to resolve full width square issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Square } from './classes/square';
 
 @Component({
@@ -8,22 +8,51 @@ import { Square } from './classes/square';
 })
 export class AppComponent implements OnInit {
   squares = new Array<Square>();
+  squareSideLength = 100;
   colours = [
     "#ffffff",
     "#2f728d",
     "#f2935d",
     "#40c0c0"
   ]
+  contentBoxStyling = {};
 
   ngOnInit() {
     this.generateSquares();
   }
 
+  @HostListener('window:orientationchange', ['$event'])
+  private onOrientationChange(event: Event) {
+    this.squares = new Array<Square>();
+    this.generateSquares();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  private onResizeEvent(event: Event) {
+    this.squares = new Array<Square>();
+    this.generateSquares();
+  }
+
+  getSquareStyling(square: Square, i: number) {
+    return {
+      'border': square.borderWidth + 'px solid',
+      'border-top-color': square.colour1,
+      'border-left-color': square.colour1,
+      'border-right-color': square.colour2,
+      'border-bottom-color': square.colour2,
+      'animation-delay': i + '0ms',
+      'background': square.colour1
+    }
+  }
+
   private generateSquares() {
+    this.contentBoxStyling = {};
     for (let i = 0; i < 1000; i++) {
       let colourPicker = Array<string>();
       this.colours.forEach(c => colourPicker.push(c));
-      this.squares.push(new Square(`square-${i}`, this.pickAColour(colourPicker), this.pickAColour(colourPicker)));
+      const borderWidth = this.determineSquareSideLength();
+      this.squareSideLength = borderWidth * 2;
+      this.squares.push(new Square(`square-${i}`, this.pickAColour(colourPicker), this.pickAColour(colourPicker), borderWidth));
     }
   }
 
@@ -31,4 +60,7 @@ export class AppComponent implements OnInit {
     const i = Math.floor(Math.random() * colourPicker.length - 1) + 1;
     return colourPicker.splice(i, 1)[0];
   }
+
+  private determineSquareSideLength = () => window.innerWidth / 20;
+
 }
--- src/app/classes/square.ts
+++ src/app/classes/square.ts
@@ -2,10 +2,12 @@ export class Square {
     name: string;
     colour1: string;
     colour2: string;
+    borderWidth: number;
 
-    constructor(name: string, colour1: string, colour2: string) {
+    constructor(name: string, colour1: string, colour2: string, borderWidth: number) {
         this.name = name;
         this.colour1 = colour1;
         this.colour2 = colour2;
+        this.borderWidth = borderWidth;
     }
 }
